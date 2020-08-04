3d8d5635f3410c433195bce8f2bfc1d81efd80d3
nicolanzi1 <nicolanzi@gmail.com>
2020-08-04 12:31:55 +0100

    Wrote approve and deny requests along with their respective methods and routes, and added buttons for these functions as well, only when there is a request to approve or deny

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -1,4 +1,9 @@
-class CatRentalRquestsController < ApplicationController
+class CatRentalRequestsController < ApplicationController
+    def approve
+        current_cat_rental_request.approve!
+        redirect_to cat_url(current_cat)
+    end
+    
     def create
         @rental_request = CatRentalRequest.new(cat_rental_request_params)
         if @rental_request.save
@@ -9,12 +14,27 @@ class CatRentalRquestsController < ApplicationController
         end
     end
 
+    def deny
+        current_cat_rental_request.deny!
+        redirect_to cat_url(current_cat)
+    end
+
     def new
         @rental_request = CatRentalRequest.new
     end
 
     private
+
+    def current_cat_rental_request
+        @rental_reques ||=
+            CatRentalRequest.includes(:cat).find(params[:id])
+    end
+
+    def current_cat
+        current_cat_rental_request.cat
+    end
+
     def cat_rental_request_params
-        params.require(:cat_rental_request).permit(:new, :create)
+        params.require(:cat_rental_request).permit(:cat_id, :end_date, :start_date, :status)
     end
 end
--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -3,10 +3,46 @@ class CatRentalRequest < ApplicationRecord
 
     validates :cat_id, :start_date, :end_date, presence: true
     validates :status, inclusion: STATUS_STATES
+    validate :start_must_come_before_end
+    validate :does_not_overlap_approved_request
 
     belongs_to :cat
     
     after_initialize :assign_pending_status
+
+    def approve!
+        raise 'not pending' unless self.status == 'PENDING'
+        transaction do
+            self.status = 'APPROVED'
+            self.save!
+            overlaping_pending_requests.each do |req|
+                req.update!(status: 'DENIED')
+            end
+        end
+    end
+
+    def approved?
+        self.status == 'APPROVED'
+    end
+
+    def denied?
+        self.status == 'DENIED'
+    end
+
+    def deny!
+        self.status = 'DENIED'
+        self.save!
+    end
+
+    def pending?
+        self.status =='PENDING'
+    end
+
+    private
+
+    def assign_pending_status
+        self.status ||= 'PENDING'
+    end
 
     def overlaping_requests
         CatRentalRequest
@@ -20,12 +56,22 @@ class CatRentalRequest < ApplicationRecord
         overlaping_requests.where('status = \'APPROVED\'')
     end
 
+    def overlaping_pending_requests
+        overlaping_requests.where('status = \'PENDING\'')
+    end
+
     def does_not_overlap_approved_request
-        return if self.deniend?
+        return if self.denied?
 
         unless overlaping_approved_requests.empty?
             errors[:base] <<
                 'Request conflicts with existing approved request'
         end
+    end
+
+    def start_must_come_before_end
+        return if start_date < end_date
+        errors[:start_date] << 'must come before end date'
+        errors[:end_date] << 'must come after start date'
     end
 end
