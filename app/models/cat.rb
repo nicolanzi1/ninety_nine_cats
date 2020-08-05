fee2e210c79610d500e340b9def6ed0a71ec4ebf
nicolanzi1 <nicolanzi@gmail.com>
2020-08-05 21:57:28 +0100

    Added User table, model, view and associations, debugged and checked authenticity token was fully working on all views

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -10,6 +10,11 @@
 #  sex         :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :integer          not null
+#
+# Indexes
+#
+#  index_cats_on_user_id  (user_id)
 #
 require 'action_view'
 
@@ -18,15 +23,28 @@ class Cat < ApplicationRecord
 
     CAT_COLORS = %w(black white orange brown).freeze
 
-    validates :color, inclusion: CAT_COLORS
-    validates :sex, inclusion: %w(M F)
-    validates :birth_date, :color, :name, :sex, presence: true
+    validates :birth_date, :color, :name, :sex, :owner, presence: true
+    validates :color, inclusion: CAT_COLORS, unless: -> { color.blank? }
+    validates :sex, inclusion: %w(M F), if: -> { sex }
+    validate :birth_date_in_the_past, if: -> { birth_date }
 
     has_many :rental_requests,
         class_name: :CatRentalRequest,
         dependent: :destroy
 
+    belongs_to :owner,
+        class_name: 'User',
+        foreign_key: :user_id
+
     def age
         time_ago_in_words(birth_date)
+    end
+
+    private
+
+    def birth_date_in_the_past
+        if birth_date && birth_date > Time.now
+            errors[:birth_date] << 'must be in the past'
+        end
     end
 end
--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -1,14 +1,33 @@
+# == Schema Information
+#
+# Table name: cat_rental_requests
+#
+#  id         :bigint           not null, primary key
+#  end_date   :date             not null
+#  start_date :date             not null
+#  status     :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  cat_id     :integer          not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_cat_rental_requests_on_cat_id   (cat_id)
+#  index_cat_rental_requests_on_user_id  (user_id)
+#
 class CatRentalRequest < ApplicationRecord
     STATUS_STATES = %w(PENDING APPROVED DENIED).freeze
 
-    validates :cat_id, :start_date, :end_date, presence: true
+    after_initialize :assign_pending_status
+
+    validates :start_date, :end_date, :status, presence: true
     validates :status, inclusion: STATUS_STATES
     validate :start_must_come_before_end
     validate :does_not_overlap_approved_request
 
     belongs_to :cat
-    
-    after_initialize :assign_pending_status
+    belongs_to :user
 
     def approve!
         raise 'not pending' unless self.status == 'PENDING'
@@ -70,8 +89,8 @@ class CatRentalRequest < ApplicationRecord
     end
 
     def start_must_come_before_end
-        return if start_date < end_date
-        errors[:start_date] << 'must come before end date'
-        errors[:end_date] << 'must come after start date'
+        errors[:start_date] << 'must specify a start date' unless start_date
+        errors[:end_date] << 'must come after start date' unless end_date
+        errors[:start_date] << 'must come before end date' if start_date > end_date
     end
 end
